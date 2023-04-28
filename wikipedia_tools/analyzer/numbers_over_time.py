ba402f4b195ef0df8c1ad34d33fda1b0b5891fe2
roxanneelbaff <roxanne.elbaff@gmail.com>
2023-04-28 15:11:07 +0200

    analaysis supports several periods

--- wikipedia_tools/analyzer/numbers_over_time.py
+++ wikipedia_tools/analyzer/numbers_over_time.py
@@ -395,7 +395,7 @@ def get_attr_for_period_as_txt(
     period=properties.DATE_YEAR,
     desc="YEAR",
 ) -> []:  ## could be categories or urls
-    periodic_df = processor.get_wikipedia_page_periodic_overview(
+    original_df = processor.get_wikipedia_page_periodic_overview(
         only_popular=only_popular, period=period, desc=desc
     )
 
@@ -403,7 +403,7 @@ def get_attr_for_period_as_txt(
         period_val = [period_val]
     res_dict = {}
     for single_period in period_val:
-        periodic_df = periodic_df[periodic_df["period"] == single_period].copy()
+        periodic_df = original_df[original_df["period"] == single_period].copy()
         periodic_df.sort_values(
             by=["revision_count", "title"], inplace=True, ascending=False
         )
